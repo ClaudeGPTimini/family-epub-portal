328621d29c409cd648a4f17d012ceb4dc56c2eed
ClaudeGPTimini <ClaudeGPTimini@users.noreply.github.com>
2026-06-11 22:17:01 -0500

    Connect request form backend

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PORTAL_CONFIG = {
-  requestEndpoint: "",
+  requestEndpoint: "https://script.google.com/macros/s/AKfycbxsnGq8nbhhmkxOynM2upEPwSNoJV09r9ggij7rSJipV-HtRsuR05HXCxjwomis0PIBOQ/exec",
   requestFormUrl: "request.html",
 };
